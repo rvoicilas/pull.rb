f16ad43b266e0eae89b36ea8f584f4213e9a3735
Radu Voicilas <radu.voicilas@gmail.com>
2013-09-22 22:52:13 +0300

    Making comment more clear

--- test/helper.rb
+++ test/helper.rb
@@ -8,7 +8,7 @@ require_relative '../lib/pull'
 
 # Execute a block of code and rescue on some
 # predefined exceptions (i.e SystemExit) so that
-# tests don't kill.
+# tests don't get killed.
 def execute_and_rescue
   begin
     yield if block_given?
